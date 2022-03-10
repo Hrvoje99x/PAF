02a59ca0ebdcfca304ed9e468825c7e7bf3fdc29
Hrvoje99x <hknezevic@pmfst.hr>
2022-03-10 13:00:12 +0100

    init

---
+++ Vjezbe_2/zadatak2.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def f():
+    F = int(input("iznos sile: "))
+    m = int(input("masa čestice: "))
+    a = F/m
+
+    t = np.arange(0.0, 10.0, 0.01)
+    x = 0
+    
