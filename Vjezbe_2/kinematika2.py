274ff78de74620d605bc70d535fa457720bb69f0
Hrvoje99x <hknezevic@pmfst.hr>
2022-03-20 14:13:24 +0100

    init

---
+++ Vjezbe_2/kinematika2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from kinematika import Kinematika
+
+Kinematika.__init__(Kinematika)
+Kinematika.jednoliko(Kinematika, 10, 5)
+Kinematika.kosi_hitac(Kinematika, 15, 45)
+
+
+
+
+
+
+
+
+
+
