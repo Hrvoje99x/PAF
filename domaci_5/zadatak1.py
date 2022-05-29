7ecac77f07c0cd3b2b11f7dbdbb39fb4d41ea83d
Hrvoje99x <hknezevic@pmfst.hr>
2022-05-29 22:31:11 +0200

    init

--- domaci_5/zadatak1.py
+++ domaci_5/zadatak1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-import em as em 
+import EM as em 
 
 def const_B(t):
 
@@ -26,15 +26,15 @@ e.reset()
 
 e = em.EM(-1, 1, np.array((0.1,0.1,0.1)), promjenjivo_B, np.array((0,0,0)), np.array((0,0,0)), 0.01)
 x2,y2,z2 = e.Euler(20)
-ax = plt.axes(projection = '3d')
+ax = plt.axes(projection='3d')
 ax.plot3D(x, y, z)
 ax.plot3D(x2, y2, z2, '-.')
 plt.show()
 
-p = em.EM(1,1,np.array((0.1,0.1,0.1)),promjenjivo_B,np.array((0,0,0)),np.array((0,0,0)),0.01)
+p = em.EM(1, 1, np.array((0.1,0.1,0.1)), promjenjivo_B, np.array((0,0,0)), np.array((0,0,0)), 0.01)
 xp,yp,zp = p.Euler(20)
 
 ax = plt.axes(projection='3d')
-ax.plot3D(x2,y2,z2)
-ax.plot3D(xp,yp,zp, '-.')
+ax.plot3D(x2, y2, z2)
+ax.plot3D(xp, yp, zp, '-.')
 plt.show()
